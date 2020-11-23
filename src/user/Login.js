a2fe7f82bb367a71d2c67dc73faf1606fefef1c4
KVexcavator <str_token@mail.ru>
2020-11-23 18:03:01 +0300

    impl Login-Logout and Register with Hook setState

--- src/user/Login.js
+++ src/user/Login.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function Login(){
+export default function Login({setUser}){
+  const [username, setUsername]= useState("")
+
+  function handleUsername (evt){
+    setUsername(evt.target.value)
+  }
+
   return(
-    <form onSubmit={e => e.preventDefault()}>
+    <form onSubmit={e => { e.preventDefault(); setUser(username) }}>
       <label htmlFor="login-username">Username: </label>
       <input 
         type="text" 
+        value={username}
+        onChange={handleUsername}
         name="login-username" 
         id="login-username" 
       />
@@ -18,6 +26,7 @@ export default function Login(){
       <input
         type="submit"
         value="Login"
+        disabled={username.lenght === 0}
       />
     </form>
   )
--- src/user/Register.js
+++ src/user/Register.js
@@ -1,29 +1,52 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function Register(){
+export default function Register({setUser}){
+  const [username, setUsername] = useState('')
+  const [password, setPassvord] = useState('')
+  const [passwordRepeat, setPassvordRepeat] = useState('')
+
+  function handlePassword (evt) {
+    setPassvord(evt.target.value)
+  }
+
+  function handlePasswordRepeat (evt) {
+    setPassvordRepeat(evt.target.value)
+  }
+
+  function handleUsername (evt) {
+    setUsername(evt.target.value)
+  }
+
   return(
-    <form onSubmit={e => e.preventDefault()} >
+    <form onSubmit={e => { e.preventDefault(); setUser(username)}} >
       <label htmlFor="register-username">Username: </label>
       <input
         type="text"
+        value={username}
+        onChange={handleUsername}
         name="register-username"
         id="register-username"
       />
       <label htmlFor="register-password">Password: </label>
       <input
         type="password"
+        value={password}
+        onChange={handlePassword}
         name="register-password"
         id="register-password"
       />
       <label htmlFor="register-password-repeat">Repeat password: </label>
       <input
         type="password"
+        value={passwordRepeat}
+        onChange={handlePasswordRepeat}
         name="register-password-repeat"
         id="register-password-repeat"
       />
       <input
         type="submit"
         value="Register"
+        disabled={username.length === 0 || password.length === 0 || password !== passwordRepeat}
       />
     </form>
   )
